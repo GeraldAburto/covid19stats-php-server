d3f239dec08d40959c1a88dce76f268be3c0ae65
Gerald Aburto Urroz <aburtogerald@gmail.com>
2020-08-19 07:36:10 -0600

    Update App.php
    Enabled CORS

--- src/App/App.php
+++ src/App/App.php
@@ -5,6 +5,22 @@ require __DIR__ . '/../../vendor/autoload.php';
 
 $app = AppFactory::create();
 
+$app->add(function (Request $request, RequestHandlerInterface $handler): Response {
+    $routeContext = RouteContext::fromRequest($request);
+    $routingResults = $routeContext->getRoutingResults();
+    $methods = $routingResults->getAllowedMethods();
+    $requestHeaders = $request->getHeaderLine('Access-Control-Request-Headers');
+
+    $response = $handler->handle($request);
+
+    $response = $response->withHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    $response = $response->withHeader('Access-Control-Allow-Origin', 'https://geraldaburto.github.io');
+    $response = $response->withHeader('Access-Control-Allow-Methods', implode(',', $methods));
+    $response = $response->withHeader('Access-Control-Allow-Headers', $requestHeaders);
+
+    return $response;
+});
+
 /* Routes */
 require __DIR__ . '/Routes.php';
 
