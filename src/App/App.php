526535e4dda9bb8447e297d517ed44075491a88c
Gerald Aburto Urroz <aburtogerald@gmail.com>
2020-08-20 14:57:33 -0600

    Modified CORS Middleware to allow multiple origins

--- src/App/App.php
+++ src/App/App.php
@@ -1,4 +1,5 @@
 <?php
+
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../../vendor/autoload.php';
@@ -6,11 +7,21 @@ require __DIR__ . '/../../vendor/autoload.php';
 $app = AppFactory::create();
 
 $app->add(function ($request, $handler) {
+    $allowedOrigins = [
+        'https://geraldaburto.github.io',
+        'http://localhost:3000'
+    ];
+
+    $origin = $request->header('origin');
+
+    if (in_array($origin, $allowedOrigins) == false)
+        $origin = 'https://geraldaburto.github.io';
+
     $response = $handler->handle($request);
     return $response
-            ->withHeader('Access-Control-Allow-Origin', 'https://geraldaburto.github.io')
-            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-            ->withHeader('Access-Control-Allow-Methods', 'GET');
+        ->withHeader('Access-Control-Allow-Origin', $origin)
+        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+        ->withHeader('Access-Control-Allow-Methods', 'GET');
 });
 
 /* Routes */
